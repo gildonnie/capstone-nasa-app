8b90d7c96a9c7032b1f488e652a44713a6c7b962
donnie gil <gildonnie864@gmail.com>
2022-08-30 15:59:20 -0700

    added client/server folders

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+//middlware
+app.use(express.urlencoded({extended: true})); // enocodes url 
+app.use(express.json()); // allows the data in post/put to be parrsed and understood by server
+app.use(logger('dev')); // sets up logging in dev (information about the requests) helps to make sure your are hitting the routes you want to hit
+app.use(express.static('public')); // before looking into routes look in this location first for html files/imgs/pdfs different files (will be changed when front end is connected)
+
+// get creates and listens for routes with certain URLs paths that is declared
+//posts routes are what recieves data from user side
+app.get('/api/posts', (req, res) => {
+  console.log('hit the route')
+  console.log(req.url)
+  res.send('test');
+});
+
+app.listen(PORT, () => {
+  console.log('server now listening');
+});
+
+// 53 min in the video
