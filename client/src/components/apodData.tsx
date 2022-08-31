64cc156896569ac735cc7f0330f2c0d4278a8f47
donnie gil <gildonnie864@gmail.com>
2022-08-31 10:25:01 -0700

    pages setup

---
+++ client/src/components/apodData.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function apodData() {
+  // apis for APOD https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+  return (
+    <div>test</div>
+  );
+}
+
+export default apodData;
